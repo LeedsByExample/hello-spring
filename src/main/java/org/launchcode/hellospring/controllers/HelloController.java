070389f8f6e6f82a10730d1f260f71ae6b3665c9
LeedsByExample <leeda.minkin@yahoo.com>
2024-01-22 14:39:36 -0600

    Worked on forms chapter exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,49 +4,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
-    //handles request at path /hello
-//    @GetMapping("hello")
+    // Handles request at path /hello
+//    @GetMapping("hello")//whatevers in parens shows path where this lives
 //    @ResponseBody
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
 
-    //now lives at /hello/goodbye
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    //handles request of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    // Handles post request from helloForm at path /hello
+    //this will be a dynamic handler (it can accept data)
+    //URL parameter and method parameter must match
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
-    //handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
+    //Handles request of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
     @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
     @GetMapping("form")
+    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action='hello' method='post'>" + //submit a request to /hello
+                "<form action='hello' method='post'>" + //action tells form to submit a request to the path /hello
                 "<input type='text' name='name'>" +
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
+
 }
 //default method for forms is GET
 //<label for="cars">Choose a car:</label>
